9c6e367ab026b446847053df0d8ad0517b1c63b9
pumpk <cong2738@gmail.com>
2025-05-29 09:43:28 +0900

    feat: color1에 대한 넓이 파라미터 추가 - color0의 넓이와 같음

--- VGA_SMPTE_color_bars/VGA_SMPTE_color_bars.srcs/sources_1/new/VGA_SMPTE_color_bars.sv
+++ VGA_SMPTE_color_bars/VGA_SMPTE_color_bars.srcs/sources_1/new/VGA_SMPTE_color_bars.sv
@@ -8,14 +8,14 @@ module VGA_SMPTE_color_bars (
     output logic [3:0] green,
     output logic [3:0] blue
 );
-    localparam COLOR0_W = 92, H_COLOR2_W = 110;
+    localparam COLOR0_W = 92, COLOR1_W = COLOR0_W, H_COLOR2_W = 110;
     localparam COLOR0_H = 360, COLOR1_H = 30, COLOR2_H = 90;
 
     logic [1:0] vertical_switch = {
         y_pixel < COLOR0_H + COLOR1_H, y_pixel < COLOR0_H
     };
     logic [2:0] color0_num = x_pixel / COLOR0_W;
-    logic [2:0] color1_num = x_pixel / COLOR0_W;
+    logic [2:0] color1_num = x_pixel / COLOR1_W;
     logic [2:0] color2_num = x_pixel / H_COLOR2_W;
 
     logic [11:0] color;
@@ -25,7 +25,7 @@ module VGA_SMPTE_color_bars (
         color = 12'dz;
         if (DE) begin
             case (vertical_switch)
-                2'b11: begin
+                2'b11 << 0: begin
                     case (color0_num)
                         0: color = 12'hfff;
                         1: color = 12'hff0;
@@ -37,7 +37,7 @@ module VGA_SMPTE_color_bars (
                     endcase
                 end
 
-                2'b10: begin
+                2'b11 << 1: begin
                     case (color1_num)
                         0: color = 12'h00f;
                         1: color = 12'h000;
@@ -51,12 +51,12 @@ module VGA_SMPTE_color_bars (
 
                 default: begin
                     case (color2_num)
-                        0: color = {4'b0000, 4'b0000, 4'b0010};
+                        0: color = 12'h002;
                         1: color = 12'hfff;
-                        2: color = {4'b0010, 4'b0000, 4'b0100};
-                        3: color = {4'b0001, 4'b0000, 4'b0000};
-                        4: color = {4'b0000, 4'b0001, 4'b0000};
-                        default: color = {4'b0000, 4'b0000, 4'b0001};
+                        2: color = 12'h208;
+                        3: color = 12'h100;
+                        4: color = 12'h010;
+                        default: color = 12'h001;
                     endcase
                 end
             endcase
